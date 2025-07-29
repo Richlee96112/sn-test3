f704ad1399a64e13dbf5aabe1bdc0de1ce91343a
Richlee96112 <gitaeklee96@gmail.com>
2025-07-28 21:29:10 -0700

    Initial Commit

---
+++ Snailer-todo/Snailer_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snailer_todoApp.swift
+//  Snailer-todo
+//
+//  Created by RichL on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Snailer_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Snailer-todoTests/Snailer_todoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Snailer_todoTests.swift
+//  Snailer-todoTests
+//
+//  Created by RichL on 7/28/25.
+//
+
+import Testing
+@testable import Snailer_todo
+
+struct Snailer_todoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
